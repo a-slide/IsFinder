de51d9c2569050448fd1c355077ecd1c465c3028
Adrien Leger <adrien.leger@inserm.fr>
2014-07-31 20:50:00 +0200

    Remove source for transfer in DNA-Tools repo

--- src/ShortReadAligner_test.py
+++ src/ShortReadAligner_test.py
@@ -1,43 +1,67 @@
-# Starndard library imports
-from os import remove
+"""
+@package AlignerSpliter
+@brief ...
+@copyright [GNU General Public License v2](http://www.gnu.org/licenses/gpl-2.0.html)
+@author Adrien Leger - 2014
+* <adrien.leger@gmail.com>
+* <adrien.leger@inserm.fr>
+* <adrien.leger@univ-nantes.fr>
+* [Github](https://github.com/a-slide)
+* [Atlantic Gene Therapies - INSERM 1089] (http://www.atlantic-gene-therapies.fr/)
+"""
 
-# Local imports
+#~~~~~~~GLOBAL IMPORTS~~~~~~~#
 
-#from AlignerSplitter import AlignerSplitter
+# Standard library packages import
+from os import remove, path
+
+# Local library packages import
 #from SamSpliter import SamSpliter
 from FastaReader import FastaReader
 from BWA.BwaIndexer import GenerateIndex, ExistingIndex
 from BWA.BwaAligner import BwaAligner
 
-def test ():
 
-    bwa_path = "bwa"
-    ref1_path = "../../test/references/rAAV_genome.fa"
-    ref2_path = "../../test/references/chr19_HRDEL.fa.gz"
-    index_path = ""
-    R1_path = "../../test/fastq/100seq_R1.fastq.gz"
-    R2_path = "../../test/fastq/100seq_R2.fastq.gz"
-    index_opt = {}
-    align_opt = {}
-    spliter_opt = {}
-    main_opt = {}
+def align  (source_list,
+            R1,
+            R2=None,
+            index = None,
+            ref_outdir = "./reference/"
+            bwa_mem = "bwa mem",
+            align_opt=None,
+            align_outdir= "./bwa_align/",
+            bwa_index = "bwa index",
+            index_opt=None,
+            index_outdir = "./bwa_index/")
+    """
+    Main function of RefMasker that integrate database creation, blast and homology masking
+    * Instantiate Blast database and blastn object
+    * Perform iterative blasts of query sequences against the subject database and create a list of
+    hits.
+    """
 
-
+    # Try to validate a index from an existing one
     try:
         if not index_path:
             raise Exception("No index provided. An index will be generated")
+
         print("Existing index provided")
-        Sources = FastaReader([ref1_path,ref2_path], write_merge=False)
-        FastaRef = FastaReader(ref1_path, ref2_path, False)
+        FastaRef = FastaReader(ref1_path, ref2_path, write_merge=False)
         Index = ExistingIndex(bwa_path, index_path)
 
+    # If no index or if an error occured during validation of the existing index = create a new one
     except Exception as E:
         print (E)
+
+        print("Merge References...")
+        mkdir(ref_outdir)
+
+        FastaRef = FastaReader([ref1_path,ref2_path], write_merge=True, output="merged.fa")
+
         print("Generating index...")
-        FastaRef = FastaReader([ref1_path,ref2_path], write_merge=False)
+        mkdir(db_outdir)
         Index = GenerateIndex(bwa_path, FastaRef.merge_ref, index_opt)
         remove (FastaRef.merge_ref)
-
 
     Aligner = BwaAligner(bwa_path, Index, align_opt)
 
